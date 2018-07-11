1d5bb612cb4edc555319cd477ab48f171eb35253
Yang Liu <foamliu@yeah.net>
2018-07-11 14:23:11 +0800

    update

--- demo.py
+++ demo.py
@@ -6,8 +6,9 @@ import cv2 as cv
 import keras
 import keras.backend as K
 import numpy as np
+import cv2 as cv
 from keras.preprocessing.image import (load_img, img_to_array)
-
+from keras.applications.vgg19 import preprocess_input
 from config import img_rows, img_cols, img_size
 from data_generator import random_crop, separate
 from model import build_model
@@ -30,30 +31,29 @@ if __name__ == '__main__':
         image_name = samples[i]
         filename = os.path.join(image_folder, image_name)
         print('Start processing image: {}'.format(filename))
-        image = load_img(filename, target_size=(img_rows, img_cols))
-        image = img_to_array(image)
-        gt = image.copy()
+        image = cv.imread(filename)
         image = random_crop(image)
+        gt = image.copy()
         x, y = separate(image)
         input = x.copy()
-        x = keras.applications.resnet50.preprocess_input(x)
+        x = cv.cvtColor(x, cv.COLOR_BGR2RGB)
+        x = preprocess_input(x)
         x_test = np.empty((1, img_size, img_size, 3), dtype=np.float32)
         x_test[0] = x
         out = model.predict(x_test)
 
         out = out[0]
+        print(out)
         out = out * 255.0
+        print(out)
         out = out.astype(np.uint8)
+        print(out)
 
         if not os.path.exists('images'):
             os.makedirs('images')
 
         output = input.copy()
         output[56:168, 56:168] = out
-
-        gt = cv.cvtColor(gt, cv.COLOR_RGB2BGR)
-        input = cv.cvtColor(input, cv.COLOR_RGB2BGR)
-        output = cv.cvtColor(output, cv.COLOR_RGB2BGR)
 
         cv.imwrite('images/{}_input.png'.format(i), input)
         cv.imwrite('images/{}_output.png'.format(i), output)
